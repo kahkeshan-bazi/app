c8625b20675ccbc5d29a9c6c78facee699997096
محمد محمدی برجک <boardgame.universe.bnd@gmail.com>
2025-08-01 15:18:42 +0330

    Update featured.js

--- js/featured.js
+++ js/featured.js
@@ -38,8 +38,7 @@ document.addEventListener('click', function (e) {
   const btn = e.target.closest('.btn-share');
   if (btn) {
     const link = btn.dataset.link;
-    const basePath = window.location.pathname.split('/')[1]; // برمی‌گردونه "app"
-const fullUrl = `${window.location.origin}/${basePath}/${link}`;
+   const fullUrl = `${window.location.origin}/app/${link}`;
     navigator.clipboard.writeText(fullUrl)
       .then(() => alert('لینک محصول کپی شد!'))
       .catch(() => alert('خطا در کپی کردن لینک.'));
